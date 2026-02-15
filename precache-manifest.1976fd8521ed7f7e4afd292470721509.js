c548bef04f67d9acae51372e3586daacb5871885
abc@gmail.com <chaithanyabommanaveni@gmail.com>
2026-02-15 09:27:15 +0530

    Updates

---
+++ precache-manifest.1976fd8521ed7f7e4afd292470721509.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92725c87c50133e3a835f55d7ec972b6",
+    "url": "/PORTFOLIO_CHAITHUYADAV/index.html"
+  },
+  {
+    "revision": "16e5b02d7f83c222e28d",
+    "url": "/PORTFOLIO_CHAITHUYADAV/static/css/main.7376647e.chunk.css"
+  },
+  {
+    "revision": "f252b0f37041f32f5bcd",
+    "url": "/PORTFOLIO_CHAITHUYADAV/static/js/2.895af72f.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/PORTFOLIO_CHAITHUYADAV/static/js/2.895af72f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16e5b02d7f83c222e28d",
+    "url": "/PORTFOLIO_CHAITHUYADAV/static/js/main.562702d8.chunk.js"
+  },
+  {
+    "revision": "fcefed550099f2e8970f",
+    "url": "/PORTFOLIO_CHAITHUYADAV/static/js/runtime-main.68aa56f2.js"
+  },
+  {
+    "revision": "395df93f3a2d8a841031e95505e05fa4",
+    "url": "/PORTFOLIO_CHAITHUYADAV/static/media/header-background.395df93f.jpg"
+  }
+]);
